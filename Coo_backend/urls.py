1511536caf34f13c3c7da1a015df9ca1871e21dc
ka609 <moussakassongo6@gmail.com>
2024-11-02 09:23:12 +0000

    initialisation du deploiement

--- Coo_backend/urls.py
+++ Coo_backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from Gestions import views as gestions_views  # Assuming 'Gestions' is your app name
 from . import views
 
@@ -31,3 +32,4 @@ urlpatterns = [
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
